b29d0f43508fc55f1b7e10be57cd73de85dc67a9
Sean Moriarity <deadlydevice@icloud.com>
2015-08-17 14:47:40 -0400

    added file for global variable to hold Parse query results
    Added a variable in the newly created Global.swift file to hold results
    from a Parse query upon application launch.

---
+++ GiveDirectly/Global.swift
@@ -0,0 +1,13 @@
+//
+//  Global.swift
+//  GiveDirectly
+//
+//  Created by Sean Moriarity on 8/17/15.
+//  Copyright (c) 2015 GiveDirectly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// initial recipient collection view data
+var recipientBrowserData = [AnyObject]()
