7c3bf2e9cd342302c85371eea2ea8b4a98188fba
deadlydevice <deadlydevice@icloud.com>
2015-08-18 16:37:57 -0400

    added delegate and protocol for button functionality

--- GiveDirectly/NewsfeedTableViewController.swift
+++ GiveDirectly/NewsfeedTableViewController.swift
@@ -8,63 +8,85 @@
 
 import UIKit
 
-class NewsfeedTableViewController: UITableViewController {
-  
-  var updateData = [AnyObject]()
-  var numberOfUpdates = Int()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+class NewsfeedTableViewController: UITableViewController, UpdateTableViewCellDelegate {
     
-    // turn off the seam on the navigation bar for this page only
-    self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "Pixel"), forBarMetrics: UIBarMetrics.Default)
-    self.navigationController?.navigationBar.shadowImage = UIImage(named: "TransparentPixel")
+    var updateData = [AnyObject]()
+    var numberOfUpdates = Int()
     
-    // autofit cells
-    tableView.estimatedRowHeight = 44
-    tableView.rowHeight = UITableViewAutomaticDimension
-    
-    //
-    self.queryParseForNewsfeedUpdates()
-    
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
-  // MARK: - Table view data source
-  
-  
-  
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return (numberOfUpdates)
-  }
-  
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    
-    let identifier = "UpdateTableViewCell"
-    let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as! UITableViewCell
-    if let updateCell = cell as? UpdateTableViewCell {
-        let updateDataForCell: AnyObject = updateData[indexPath.row]
-        updateCell.configureUpdateTableViewCell(updateDataForCell)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // turn off the seam on the navigation bar for this page only
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "Pixel"), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage(named: "TransparentPixel")
+        
+        // autofit cells
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        //
+        self.queryParseForNewsfeedUpdates()
+        
     }
     
-    return cell
-    
-  }
-  
-  func queryParseForNewsfeedUpdates() {
-    
-    // construct query to return target recipient
-    let query:PFQuery = PFQuery(className: "RecipientUpdates")
-    query.orderByAscending("createdAt")
-    query.findObjectsInBackgroundWithBlock { (result: [AnyObject]?, error: NSError?) -> Void in
-      self.updateData = result!
-      self.numberOfUpdates = result!.count
-      self.tableView?.reloadData()
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-  }
-  
-  
+}
+
+// MARK: - Table View Data Source
+extension NewsfeedTableViewController {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (numberOfUpdates)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let identifier = "UpdateTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier) as! UITableViewCell
+        if let updateCell = cell as? UpdateTableViewCell {
+            let updateDataForCell: AnyObject = updateData[indexPath.row]
+            updateCell.configureUpdateTableViewCell(updateDataForCell)
+        }
+        
+        return cell
+    }
+    
+    func queryParseForNewsfeedUpdates() {
+        
+        // construct query to return target recipient
+        let query:PFQuery = PFQuery(className: "RecipientUpdates")
+        query.orderByAscending("createdAt")
+        query.findObjectsInBackgroundWithBlock { (result: [AnyObject]?, error: NSError?) -> Void in
+            self.updateData = result!
+            self.numberOfUpdates = result!.count
+            self.tableView?.reloadData()
+        }
+    }
+}
+
+// MARK: UpdateTableViewCellDelegate
+extension NewsfeedTableViewController: UpdateTableViewCellDelegate {
+    
+    func updateLikeButtonDidTap(cell: UpdateTableViewCell, sender: AnyObject) {
+        // TODO: implement like functionality
+        
+        // create a Liked object with the user's objectId
+        // that points to the update (fromUser to likedPost)
+    }
+    
+    func updateCommentButtonDidTap(cell: UpdateTableViewCell, sender: AnyObject) {
+        // TODO: implement comment functionality
+        
+        // create a Comment object with the user's objectId
+        // that points to the update (author to relatedUpdate with text)
+    }
+    
+    func updateExtraButtonDidTap(cell: UpdateTableViewCell, sender: AnyObject) {
+        // TODO: implement extra functionality
+        
+        // open a view that allows user to report, share, etc.
+    }
+    
 }
--- GiveDirectly/UpdateTableViewCell.swift
+++ GiveDirectly/UpdateTableViewCell.swift
@@ -8,81 +8,127 @@
 
 import UIKit
 
+protocol UpdateTableViewCellDelegate: class {
+    func updateLikeButtonDidTap(cell: UpdateTableViewCell, sender: AnyObject)
+    func updateCommentButtonDidTap(cell: UpdateTableViewCell, sender: AnyObject)
+    func updateExtraButtonDidTap(cell: UpdateTableViewCell, sender: AnyObject)
+}
+
 class UpdateTableViewCell: UITableViewCell {
-  
-  
-  @IBOutlet weak var authorImageView: UIImageView!
-  @IBOutlet weak var authorNameLabel: UILabel!
-  @IBOutlet weak var updateTitleLabel: UILabel!
-  @IBOutlet weak var timestampLabel: UILabel!
-  @IBOutlet weak var updateTextView: UITextView!
-  @IBOutlet weak var numberOfLikesLabel: UILabel!
-  @IBOutlet weak var numberOfComments: UILabel!
-  
-  
-  override func awakeFromNib() {
-    super.awakeFromNib()
-    // Initialization code
-  }
-  
-  override func setSelected(selected: Bool, animated: Bool) {
-    super.setSelected(selected, animated: animated)
     
-    // Configure the view for the selected state
+    weak var delegate: UpdateTableViewCellDelegate?
     
-  }
-  
-  
-  func configureUpdateTableViewCell(updateData: AnyObject) {
     
-    // have to get the updateData[indexPath.row] in table view controller for subscripting to work like below...
+    @IBOutlet weak var authorImageView: UIImageView!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var updateTitleLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var updateTextView: UITextView!
+    @IBOutlet weak var numberOfLikesLabel: UILabel!
+    @IBOutlet weak var numberOfCommentsLabel: UILabel!
     
-    // configure outlets with Parse data
-    let author:String! = (updateData as AnyObject)["GDID"] as! String
-    // check Recipients class to mine information
-    var query = PFQuery(className:"Recipients")
-    query.whereKey("gdid", equalTo: author)
-    query.findObjectsInBackgroundWithBlock {
-        (objects: [AnyObject]?, error: NSError?) -> Void in
-        if error == nil {
-            // check to see if something exists
-            if let objects = objects as? [PFObject] {
-                for object in objects {
-                    // if name is pulled
-                    if let recipientName = object["firstName"] as? String {
-                        self.authorNameLabel.text = recipientName
-                    }
-                    // TODO: create optional else statement
-                    // if image is pulled
-                    if let recipientProfilePhoto = object["image"] as? PFFile {
-                        recipientProfilePhoto.getDataInBackgroundWithBlock {
-                            (imageData: NSData?, error: NSError?) -> Void in
-                            if (error == nil) {
-                                let image = UIImage(data: imageData!)
-                                self.authorImageView.image = image
-                                self.authorImageView.layer.cornerRadius = self.authorImageView.frame.size.width / 2
-                                self.authorImageView.clipsToBounds = true
+    @IBOutlet weak var likeButton: SpringButton!
+    @IBOutlet weak var commentButton: SpringButton!
+    @IBOutlet weak var extraButton: SpringButton!
+
+    // TODO: assign Parse data to numberOfLikesLabel and numberOfCommentsLabel
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+        
+        // TODO: configure labels for number of likes and comments
+    }
+    
+    @IBAction func likeButtonDidTap(sender: AnyObject) {
+        
+        likeButton.animation = "pop"
+        likeButton.force = 3
+        likeButton.animate()
+        
+        // update the numberOfLikesLabel with the user's like
+//        numberOfLikesLabel.text = "1"
+        
+        delegate?.updateLikeButtonDidTap(self, sender: sender)
+    }
+    
+    @IBAction func commentButtonDidTap(sender: AnyObject) {
+        
+        commentButton.animation = "pop"
+        commentButton.force = 3
+        commentButton.animate()
+        
+        delegate?.updateCommentButtonDidTap(self, sender: sender)
+    }
+    
+    @IBAction func extraButtonDidTap(sender: AnyObject) {
+        
+        extraButton.animation = "pop"
+        extraButton.force = 3
+        extraButton.animate()
+        
+        delegate?.updateExtraButtonDidTap(self, sender: sender)
+    }
+    
+    
+    
+    func configureUpdateTableViewCell(updateData: AnyObject) {
+        
+        // have to get the updateData[indexPath.row] in table view controller for subscripting to work like below...
+        
+        // configure outlets with Parse data
+        let author:String! = (updateData as AnyObject)["GDID"] as! String
+        // check Recipients class to mine information
+        var query = PFQuery(className:"Recipients")
+        query.whereKey("gdid", equalTo: author)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                // check to see if something exists
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        // if name is pulled
+                        if let recipientName = object["firstName"] as? String {
+                            self.authorNameLabel.text = recipientName
+                        }
+                        // TODO: create optional else statement
+                        // if image is pulled
+                        if let recipientProfilePhoto = object["image"] as? PFFile {
+                            recipientProfilePhoto.getDataInBackgroundWithBlock {
+                                (imageData: NSData?, error: NSError?) -> Void in
+                                if (error == nil) {
+                                    let image = UIImage(data: imageData!)
+                                    self.authorImageView.image = image
+                                    self.authorImageView.layer.cornerRadius = self.authorImageView.frame.size.width / 2
+                                    self.authorImageView.clipsToBounds = true
+                                }
                             }
                         }
                     }
                 }
+            } else {
+                // log details of the failure
+                println("Error: \(error!) \(error!.userInfo!)")
             }
-        } else {
-            // log details of the failure
-            println("Error: \(error!) \(error!.userInfo!)")
         }
+        
+        let title:String? = (updateData as AnyObject)["method"] as? String
+        let updateText:String? = (updateData as AnyObject)["life_difference"] as? String
+        
+        // get the date and format
+        let date:String = (updateData as AnyObject)["date"] as! String
+        var newDate = date.substringToIndex(advance(date.endIndex, -18))
+        
+        // assign labels and views
+        self.updateTitleLabel.text = "via " + title!
+        self.updateTextView.text = updateText
+        self.timestampLabel.text = newDate
     }
-    
-    let title:String? = (updateData as AnyObject)["method"] as? String
-    let updateText:String? = (updateData as AnyObject)["life_difference"] as? String
-    
-    // get the date and format
-    let date:String = (updateData as AnyObject)["date"] as! String
-    var newDate = date.substringToIndex(advance(date.endIndex, -18))
-    
-    // assign labels and views
-    self.updateTitleLabel.text = "via " + title!
-    self.updateTextView.text = updateText
-    self.timestampLabel.text = newDate
-  }
 }
