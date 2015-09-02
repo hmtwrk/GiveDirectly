e9c223ee3490aef73aa1953e33c72ba8ab74b2e5
deadlydevice <deadlydevice@icloud.com>
2015-09-02 13:37:29 -0400

    tweaked font size for recipient stories text views
    increased the font size to 15 to match the size of newsfeed items

--- GiveDirectly/Recipient.swift
+++ GiveDirectly/Recipient.swift
@@ -82,6 +82,7 @@ class Recipient {
         //         Parse API call to return target recipient
         let query:PFQuery = PFQuery(className: "Recipients")
         query.orderByAscending("createdAt")
+        // the Parse default query limit is 100 (there are now almost 300 recipients)
 //        query.limit = 20
         query.findObjectsInBackgroundWithBlock { (result: [AnyObject]?, error: NSError?) -> Void in
             recipientBrowserData = result!
